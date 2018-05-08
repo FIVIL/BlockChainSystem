0d70e490448c6be37fe905e9b41d3c75bcbd56b6
FIVIL <31272779+FIVIL@users.noreply.github.com>
2018-05-08 10:42:21 +0430

    Add project files.

---
+++ BlockChainSystem/Block.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlockChainSystem
+{
+    class Block
+    {
+        public Block()
+        {
+            
+        }
+    }
+}
