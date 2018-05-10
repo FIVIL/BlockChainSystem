f6bea2bc6fb4b706651cd7387fea71a2fae8b36f
FIVIL <31272779+FIVIL@users.noreply.github.com>
2018-05-10 18:40:29 +0430

    utlities added.

--- BlockChainSystem/Block.cs
+++ BlockChainSystem/Block.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using CryptoApi;
+using TransactionManager;
+using Blake2Sharp;
 
 namespace BlockChainSystem
 {
---
+++ BlockChainSystem/Utlities.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Blake2Sharp;
+
+namespace BlockChainSystem
+{
+    public static class Utlities
+    {
+        public static string GetHashStringBlacke2b(this string clearstring)
+        {
+            return Convert.ToBase64String(Blake2B.ComputeHash(Blake2B.ComputeHash(Encoding.UTF8.GetBytes(clearstring))));
+        }
+    }
+}
